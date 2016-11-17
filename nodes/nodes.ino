f4200bc59b480cc4b58c3edfbb072c3af61eeb31
Eric Mooney <eric.c.mooney@gmail.com>
2016-11-17 13:15:16 -0500

    refactor button press

--- nodes/nodes.ino
+++ nodes/nodes.ino
@@ -11,25 +11,11 @@ Zombie nodes
 
 */
 
-//Define the relevant pins
+// Define the relevant pins
 #define blueLedPin 5
 #define greenLedPin 6
 #define redLedPin 7
 #define buttonPin 8
-
-
-int getUniqueID() {
-  // Generates unique ID on startup
-  // From http://stackoverflow.com
-  // /questions/21559264/unique-machine-id-for-arduino-project
-  uint32_t checksum = 0;
-  for(uint16_t u = 0; u < 2048; u++)
-  {
-    //checksum += the byte number u in the ram
-    checksum += * ( (byte *) u );
-  }
-  return checksum;
-}
 
 // Variables will change:
 int blueLedState  = HIGH;    // current state of output pin
@@ -42,10 +28,21 @@ int lastButtonState = LOW;   // the previous reading from input pin
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 2;    // the debounce time; increase if the output flickers
 
-void setup() {
-  // Get my unique ID
-  int ID = getUniqueID();
 
+// Generates unique ID on startup
+// From http://stackoverflow.com/questions/21559264/unique-machine-id-for-arduino-project
+int getUniqueID() {
+  uint32_t checksum = 0;
+  for(uint16_t u = 0; u < 2048; u++)
+  {
+    //checksum += the byte number u in the ram
+    checksum += * ( (byte *) u );
+  }
+  return checksum;
+}
+
+// initialize digital pins
+void initializePins() {
   // initialize digital pins with LEDs as an outputs.
   pinMode(blueLedPin,  OUTPUT);
   pinMode(greenLedPin, OUTPUT);
@@ -53,23 +50,20 @@ void setup() {
   
   // initialize the button as an input.
   pinMode(buttonPin, INPUT);
-
+  
   // set initial LED state
   digitalWrite(blueLedPin,  blueLedState);
   digitalWrite(greenLedPin, greenLedState);
   digitalWrite(redLedPin,   redLedState);
 
-  Serial.begin(9600);
-  Serial.println(ID, HEX);
 }
 
-
-void loop() {
-
-// ----------Button Handling---------- //  
-  // read the state of the switch into a local variable:
+// Check to see if the button has been pressed since last loop
+int checkButtonPress(){
+    // read the state of the switch into a local variable:
   int reading = digitalRead(buttonPin);
-
+  int buttonPressed = 0;
+  
   // check to see if you just pressed the button
   // (i.e. the input went from LOW to HIGH),  and you've waited
   // long enough since the last press to ignore any noise:
@@ -90,19 +84,49 @@ void loop() {
 
       // only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
-        blueLedState = !blueLedState;
-        greenLedState = !greenLedState;
-        redLedState = !redLedState;
+        buttonPressed = 1;
       }
     }
   }
 
-  // set the LED:
-  digitalWrite(blueLedPin, blueLedState);
-  digitalWrite(greenLedPin, greenLedState);
-  digitalWrite(redLedPin, redLedState);
-
   // save the reading.  Next time through the loop,
   // it'll be the lastButtonState:
   lastButtonState = reading;
+
+  return buttonPressed;
+}
+
+// When button is pressed, handle it
+void handleButtonPress() {
+    blueLedState = !blueLedState;
+    greenLedState = !greenLedState;
+    redLedState = !redLedState;
+
+    // set the LED:
+    digitalWrite(blueLedPin, blueLedState);
+    digitalWrite(greenLedPin, greenLedState);
+    digitalWrite(redLedPin, redLedState);
+}
+
+
+void setup() {
+  // Get my unique ID
+  int ID = getUniqueID();
+
+  // Initialize digital pins
+  initializePins();
+ 
+  Serial.begin(9600);
+  Serial.println(ID, HEX);
+}
+
+void loop() {
+
+  // ----------Button Handling---------- //  
+  int was_pressed = checkButtonPress();
+  
+  if(was_pressed) {
+    handleButtonPress();
+  }
+  
 }
